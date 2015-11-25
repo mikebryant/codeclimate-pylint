ae2fe4d026022298aa8f4196253596ceaf8523e5
Mike Bryant <mike@mikebryant.me.uk>
2015-11-25 22:08:31 +0000

    Change the error logic to include exit code 1

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -x
 
 pylint -f codeclimate_reporter.CodeClimateReporter `ls -1 */__init__.py | cut -d '/' -f 1` *.py
 PYLINT_EXIT=$?
-if [ $PYLINT_EXIT -ge 32 ]
+if [ $PYLINT_EXIT -ge 32 ] || [ $PYLINT_EXIT -eq 1 ]
 then
   exit $PYLINT_EXIT
 fi
